e9c656c809ecd9243ae2f4ca2c12865811724734
med-abouchouar <abouchouar98@gmail.com>
2021-02-13 15:47:05 +0100

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing does not get updated !
+          Docker make life easy :)
         </p>
         <a
           className="App-link"
