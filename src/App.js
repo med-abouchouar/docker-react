eb746ed6135c9fe342a3f295bb729a9fb0f6ae3d
med-abouchouar <abouchouar98@gmail.com>
2021-02-13 17:01:39 +0100

    deploy to aws beanstalk docker using travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker make life easy :) with AWS !
+          Docker make life easy :) with AWS 
         </p>
         <a
           className="App-link"
